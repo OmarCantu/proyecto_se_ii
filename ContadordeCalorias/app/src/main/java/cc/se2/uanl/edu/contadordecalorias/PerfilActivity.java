cc6bf20677654a3cbc31061e32ce27930a706751
Omar Cantu <omar.a.cm@hotmail.com>
2015-02-14 12:37:42 -0600

    Strings y colores corregidos. Ahora en res/values.
    Strings y colores corregidos. Ahora en están en res/values en lugar de
    estar hardcodeados. Archivos innecesarios borrados.

--- ContadordeCalorias/app/src/main/java/cc/se2/uanl/edu/contadordecalorias/PerfilActivity.java
+++ ContadordeCalorias/app/src/main/java/cc/se2/uanl/edu/contadordecalorias/PerfilActivity.java
@@ -18,6 +18,19 @@ public class PerfilActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        /*// Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);*/
     }
 
     @Override
